81d69bf443643e7ca6f7087d54982884f01a25d6
Austin <austin.ivey33@gmail.com>
2021-07-12 18:55:49 -0400

    Adding Index and JS

---
+++ srcipt.js
@@ -0,0 +1,47 @@
+// MVP
+// Make namespace App
+
+
+// Header Nav section//
+// 
+// Access live JS method to get current Date and Time
+//  
+// Weather API section
+// 
+// Make sure Weather API is good and able to recieve data we require
+// Get API weekly data
+// Store API data into useable Variables
+// append weekly API Data onto DOM
+// 
+// AC control unit Section
+
+// Display a number in degrees
+// Store that number in a let variable
+// on click of + or - increase or decrease the number on the DOM
+// if statement that warns the user that this is as low as it can go or as high
+// 
+
+// MVP END//
+// 
+// Stretch Begins//
+// 
+//
+// Side Nav Section//
+// on click of font awesome Icons it will expand 1/4 sections to full screen and remove the other 3 sections (or however many sections we finish)
+// 
+// Door Locking Section//
+// 
+// list of doors in house
+// user can add a door if in section or remove a door if in section
+// on click transform before and after elements to create a toggle effect
+// 
+// Lighting Section
+// 
+// list of lights in house
+// user can add a light if in section or remove a light if in section
+// on click transform before and after elements to create a toggle effect
+// 
+// News and Task list Section //
+// have some default task lists in section
+// if clicked toggle done or not done state
+// news storys added to bottom of section through an API
